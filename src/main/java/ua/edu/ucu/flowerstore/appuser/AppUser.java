0d592487b542efaba04950e390e2ab25a757aac0
Roman <romanmutel@gmail.com>
2022-11-14 14:32:42 +0200

    reverted changed

--- src/main/java/ua/edu/ucu/flowerstore/appuser/AppUser.java
+++ src/main/java/ua/edu/ucu/flowerstore/appuser/AppUser.java
@@ -3,8 +3,12 @@ package ua.edu.ucu.flowerstore.appuser;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Transient;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 import java.time.Period;
 
